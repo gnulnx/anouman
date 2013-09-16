083cfa74f2fe98756f62b28212548cb98c3aa2e3
john furr <jfurr@johns-MacBook-Pro.local>
2013-09-16 11:36:27 -0400

    removed code from anouman/templates/__init__.py

--- anouman/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-template_dir = os.path.dirname(__file__)
-def gunicorn_start(context):
-    with open('%s/gunicorn_start.template'%(template_dir), 'r') as f:
-        return f.read() % context
