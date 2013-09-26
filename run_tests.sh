ee3d63b2322dd3159e37be7c452b877b9e2f090e
john furr <jfurr@johns-MacBook-Pro.local>
2013-09-26 19:01:32 -0400

    Modifications to run_tests.sh...command line switches

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
 
-workon anouman
+#############################################
+#                                           #
+#   Simple Test Driver                      #
+#                                           # 
+#   ./run_tests  -q (queit)  -f (failfast)  #
+#############################################
 
-/usr/bin/yes | python -m unittest discover -v --failfast
-#/usr/bin/yes | python -m unittest discover -v > /dev/null
+#QUEIT=""
+#if [[ "$@" == *-q* ]]; then
+#    QUEIT=" > /dev/null"
+#fi
+#
+#FAILFAST=""
+#if [[ "$@" == *-f* ]]; then
+#    FAILFAST=" --failfast "
+#fi
+#
+#VERBOSE=""
+#if [[ "$@" == *-v* ]]; then
+#    VERBOSE=" -v "
+#fi
+#
+#echo "/usr/bin/yes | python -m unittest discover $VERBOSE $FAILFAST  $QUEIT"
+#exec /usr/bin/yes | python -m unittest discover $VERBOSE $FAILFAST $QUEIT
+
+
+
+if [[ "$@" == *-q* ]];
+then
+    if [[ "$@" == *-f* ]];
+    then
+        /usr/bin/yes | python -m unittest discover -v --failfast  > /dev/null
+    else
+        /usr/bin/yes | python -m unittest discover -v > /dev/null
+    fi
+else
+    if [[ "$@" == *-f* ]];
+    then
+        /usr/bin/yes | python -m unittest discover -v --failfast
+    else
+        /usr/bin/yes | python -m unittest discover -v 
+    fi
+fi
