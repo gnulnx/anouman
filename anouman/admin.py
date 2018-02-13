239bdf5091587e04a52baf6eecb392e28afa1152
John Furr <jfurr@Johns-MacBook-Pro.local>
2018-02-12 20:18:32 -0500

    rearragne...still broke

--- anouman/admin.py
+++ anouman/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from cloudgroup.models import SSHKey, IPAddress, Machine, CloudGroup
+from anouman.models import SSHKey, IPAddress, Machine, CloudGroup
 
 
 class SSHKeyAdmin(admin.ModelAdmin):
